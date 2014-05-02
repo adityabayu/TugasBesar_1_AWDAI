66324063bb599149fffc9b993eeff50a08f005da
Andre Rizkiana Wandira <yuuzuzero@gmail.com>
2014-05-02 21:39:15 +0700

    update home.php
    yg counter lebih enak gini dit.
    gpp ye, hehe

--- home.php
+++ home.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<title>home</title>
 <style>
 p.padding
 {
@@ -10,6 +12,35 @@ padding-left:50px;
 color:#612915;
 }
 </style>
+
+	<style type="text/css">
+	@import "jquery.countdown.css";
+	#defaultCountdown { width: 240px; height: 45px; }
+	#defaultCountUp { width: 240px; height: 45px; }
+	</style>
+	<script type="text/javascript" src="jquery.min.js"></script>
+	<script type="text/javascript" src="jquery.countdown.js"></script>
+	<script type="text/javascript">
+<!-- austDay.getFullYear() menghasilkan tahun saat ini ; austDay.getFullYear() +1 menghasilkan tahun depan,misal 2013+1 =2014  -->
+<!--0 menghasilkan bulan pertama-->
+<!--1 menghasilkan hari pertama-->
+	$(function () {
+	var austDay = new Date();
+	austDay = new Date(austDay.getFullYear() + 0, 4, 5);
+	$('#defaultCountdown').countdown({until: austDay});
+	$('#year').text(austDay.getFullYear());
+	});
+	</script>
+		<script type="text/javascript">
+<!-- austDay.getFullYear() menghasilkan tahun saat ini ; misal 2013 -->
+<!--0 merupakan bulan pertama,1 merupakan hari pertama-->
+	$(function () {
+	var austDay = new Date();
+	austDay = new Date(austDay.getFullYear(), 4 ,0);
+	$('#defaultCountUp').countdown({since: austDay});
+	$('#year').text(austDay.getFullYear());
+	});
+	</script>
 </head>
 <body>
 	  <p class="padding">
